bba81997954c2b5b45e207e737aa444b54fb6f10
hnialatifa@gmail.com <hnialatifa@gmail.com>
2022-10-31 22:40:48 +0100

    add certificate

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -11,8 +11,14 @@ const routes: Routes = [
   { path: 'test-error', component: TestErrorComponent, data: { breadcrumb: 'Test Error' } },
   { path: 'server-error', component: ServerErrorComponent, data: { breadcrumb: 'Server Error' }},
   { path: 'not-found', component: NotFoundComponent },
-  { path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule), data: { breadcrumb: 'Shop' } },
-  //{ path: 'basket', loadChildren: () => import('./basket/basket.module').then(mod => mod.BasketModule), data: { breadcrumb: 'Basket' } },
+  { path: 'shop', 
+         loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule),
+         data: { breadcrumb: 'Shop' } 
+  },
+  { path: 'basket', 
+        loadChildren: () => import('./basket/basket.module').then(mod => mod.BasketModule), 
+        data: { breadcrumb: 'Basket' } 
+  },
   { path: '**', redirectTo: 'not-found', pathMatch: 'full' },
 ];
 
