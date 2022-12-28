ab0ca0e72ac6b06c7407b6c0bbf100e98ead1a3e
Zied kalghoum <zied.kalghoum@gmail.com>
2022-12-28 14:50:44 +0100

    add swagger autorizationh

--- API/Controllers/BasketController.cs
+++ API/Controllers/BasketController.cs
@@ -1,4 +1,6 @@
 using System.Threading.Tasks;
+using API.Dtos;
+using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
 using Microsoft.AspNetCore.Mvc;
@@ -6,9 +8,11 @@ using Microsoft.AspNetCore.Mvc;
 namespace API.Controllers {
     public class BasketController : BaseApiController {
         private readonly IBasketRepository _basketRepository;
+        private readonly IMapper _mapper;
 
-        public BasketController (IBasketRepository basketRepository) {
+        public BasketController (IBasketRepository basketRepository, IMapper mapper) {
             _basketRepository = basketRepository;
+            _mapper = mapper;
         }
 
         [HttpGet]
@@ -19,8 +23,9 @@ namespace API.Controllers {
         }
 
         [HttpPost]
-        public async Task<ActionResult<CustomerBasket>> UpdateBasket (CustomerBasket basket) {
-            var customerBasket = await _basketRepository.UpdateBasketAsync (basket);
+        public async Task<ActionResult<CustomerBasket>> UpdateBasket (CustomerBasketDto basket) 
+        {
+            var customerBasket = _mapper.Map<CustomerBasketDto, CustomerBasket>(basket);
 
             var updatedBasket = await _basketRepository.UpdateBasketAsync (customerBasket);
 
--- API/Extensions/SwaggerServiceExtensions.cs
+++ API/Extensions/SwaggerServiceExtensions.cs
@@ -9,14 +9,30 @@ namespace API.Extensions
         public static IServiceCollection AddSwaggerDocumetation (this IServiceCollection services) {
             services.AddSwaggerGen (c => {
                 c.SwaggerDoc ("v1", new OpenApiInfo { Title = "Jungle API", Version = "v1" });
+                 var securitySchema = new OpenApiSecurityScheme
+                {
+                    Description = "JWT Auth Bearer Scheme",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "Bearer",
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                };
+
+                c.AddSecurityDefinition("Bearer", securitySchema);
+                var securityRequirement = new OpenApiSecurityRequirement{{securitySchema, new[] {"Bearer"}}};
+                c.AddSecurityRequirement(securityRequirement);
             });
             return services;
         }
         public static IApplicationBuilder  UseSwaggerDocumentation(this IApplicationBuilder app){
             app.UseSwagger ();
             app.UseSwaggerUI (c => {
-                c
-                    .SwaggerEndpoint ("/swagger/v1/swagger.json", "Jungle API v1");
+                c.SwaggerEndpoint ("/swagger/v1/swagger.json", "Jungle API v1");
             });
             return app;
         }
